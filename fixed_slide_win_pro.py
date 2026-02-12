c4fef6cb8d2927e01fd1c6c9cc5b866dc91eb008
ctsnguyen <ctsnguyen@ucdavis.edu>
2026-02-11 20:58:52 -0800

    sliding window checkpoint

--- fixed_slide_win_pro.py
+++ fixed_slide_win_pro.py
@@ -41,7 +41,7 @@ def main():
       
     base = 0          # last acknowledged byte
     data_index = 0
-    dpp_list = []
+    # dpp_list = []
     # initialize sender's socker
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sag_socket:
         # Start Throughput Measurement Here
@@ -51,16 +51,21 @@ def main():
 
         print("SAG Sending...")
 
-        while data_index < len(mp3_bytes):
+        while base < len(mp3_bytes):
             print("Entering Loop")
-            # send data in packet to receiver
-            payload = bytes(mp3_bytes[data_index:data_index+1])   # .encode()
-            packet = format_packet(base, payload)
 
-            print(f"Sending seq={base}, data='{payload[0]}'")
-            sag_socket.sendto(packet, (UDP_IP, UDP_PORT))
+            while data_index < len(mp3_bytes) and data_index < base + WINDOW_SIZE:
+                # send data in packet to receiver
+                payload = bytes(mp3_bytes[data_index:data_index+1])
+                packet = format_packet(data_index, payload)
+
+                print(f"Sending seq={base}, data='{payload[0]}'")
+                sag_socket.sendto(packet, (UDP_IP, UDP_PORT))
+
+                data_index += 1
+
             # Delay Per Packet Timer Starts Here
-            dpp_timer_start = time.perf_counter()
+            # dpp_timer_start = time.perf_counter()
 
             # receive ACK from receiver
             try:
@@ -70,22 +75,27 @@ def main():
                 )
                 
                 # Received ACK, stop timer
-                dpp_timer_end = time.perf_counter()
-                print(f"ACK received: {ack_seq}")
-                print(f"Delay Per Packet Time: {(dpp_timer_start - dpp_timer_end):.4f}")
-                dpp_list.append(dpp_timer_start - dpp_timer_end)
+                # dpp_timer_end = time.perf_counter()
 
-                # Store new last ack
-                if ack_seq == base + 1: # len(payload)
+                print(f"ACK received: {ack_seq}")
+
+                # print(f"Delay Per Packet Time: {(dpp_timer_start - dpp_timer_end):.4f}")
+                # dpp_list.append(dpp_timer_start - dpp_timer_end)
+
+                # Move window forward
+                if ack_seq > base: # len(payload)
                     base = ack_seq
-                    data_index += 1
                     # print(f"Sending Message: {mp3_bytes[data_index]}")
-                    print("Sent!")
-                else:
-                    print("Unexpected ACK, resending...")
+                    print("Window moved forward!")
 
             except socket.timeout:
                 print("Timeout — resending packet")
+
+                # Resent all packets in window
+                for i in range(base, data_index):
+                    payload = bytes(mp3_bytes[i:i+1])
+                    packet = format_packet(i, payload)
+                    sag_socket.sendto(packet, (UDP_IP, UDP_PORT))
         
         throughput_timer_end = time.perf_counter()
         sag_socket.sendto(format_packet(base, b'==FINACK=='), (UDP_IP, UDP_PORT))
